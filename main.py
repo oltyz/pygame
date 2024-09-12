6f84a70470256cecbc6f6644fcfbbfdceffbd1e7
oltyz <oltyz@yandex.ru>
2024-09-12 14:08:53 +0300

    all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,19 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load('')
+icon = pygame.image.load('IMG_3862.JPG')
+pygame.display.set_icon(icon)
+
+
+target_image = pygame.image.load("img/target.png")
+target_width = 150
+target_height = 195
+
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+
 
 
 running = True
